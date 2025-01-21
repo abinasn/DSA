e4ffdcd53dbd402a351d503cea353d3104ce9f74
Abinas Patra <2010abinas@gmail.com>
2025-01-21 14:32:07 +0530

    removed searching

--- Searching/liner.js
+++
@@ -1,9 +0,0 @@
-function linerSearch(arr, item) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] == item) {
-            return i;
-        }
-    }
-    return -1;
-}
-console.log(linerSearch([1, 2, 3, 4, 5], 44));
